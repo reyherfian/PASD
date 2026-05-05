03f422a41f97ef024d90c11ef5cb79e3277a03cb
reyherfian <rey.adriano.m@gmail.com>
2026-05-05 11:51:31 +0700

    percobaan 2

--- Jobsheet9/MahasiswaDemo03.java
+++ Jobsheet9/MahasiswaDemo03.java
@@ -36,6 +36,8 @@ public class MahasiswaDemo03 {
                         int nilai = sc.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai Biner Tugas: " + biner);
                     }
                     break;
                 case 3:
--- Jobsheet9/StackTugasMahasiswa03.java
+++ Jobsheet9/StackTugasMahasiswa03.java
@@ -77,4 +77,18 @@ public class StackTugasMahasiswa03 {
             return 0;
         }
     }
+
+    public String konversiDesimalKeBiner (int nilai) {
+        StackKonversi03 stack = new StackKonversi03();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
